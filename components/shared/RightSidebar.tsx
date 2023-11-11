93581f436404116548650f62586a1f9e22756440
Shakil <shakil11271@gmail.com>
2023-11-11 21:59:17 +0600

    solve width problem

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,9 @@
+function RightSidebar() {
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px]">
+      RightSidebar
+    </section>
+  );
+}
+
+export default RightSidebar;
