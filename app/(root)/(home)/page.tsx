74d3462afdb61d8100f825341d949d5201833531
jhshakil <shakil11271@gmail.com>
2023-11-23 01:17:45 +0600

    complete 23.19

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
@@ -7,32 +8,40 @@ import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 const questions = [
-  // {
-  //   _id: 1,
-  //   title: "Cascading Deletes in SQLAlchemy?",
-  //   tags: [
-  //     { _id: 1, name: "python" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: 'John Doe',
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: '2021-09-01T12:00:00.000Z'
-  // },
-  // {
-  //   _id: 2,
-  //   title: "How to create a div?",
-  //   tags: [
-  //     { _id: 1, name: "css" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: 'John Doe',
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: '2021-09-01T12:00:00.000Z'
-  // },
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "john-doe.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to create a div?",
+    tags: [
+      { _id: "3", name: "css" },
+      { _id: "4", name: "html" },
+    ],
+    author: {
+      _id: "2",
+      name: "John Smith",
+      picture: "john-smith.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
 ];
 
 export default function Home() {
@@ -66,15 +75,27 @@ export default function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "question card")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There is no question to show"
             description="Be the first to break the silence! Ask a Question and kickstart the
           discussion. our query could be the next big thing others learn from. Get
           involved!"
-          link="/"
-          linkTitle="Ask a Question"
+            link="/"
+            linkTitle="Ask a Question"
           />
         )}
       </div>
---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,41 @@
+interface Props {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvotes: number;
+  views: number;
+  answers: Array<Object>;
+  createdAt: Date;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvotes,
+  views,
+  answers,
+  createdAt,
+}: Props) => {
+  return (
+    <div className="card-wrapper p-9 sm:px-11 rounded-[10px]">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span>{String(createdAt)}</span>
+        </div>
+      </div>
+      {title}
+    </div>
+  );
+};
+
+export default QuestionCard;
