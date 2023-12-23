340cb637cd57a1220c798484d0f55908f2313b98
jhshakil <shakil11271@gmail.com>
2023-12-23 22:47:03 +0600

    undo all change

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,4 +1,4 @@
-// import Question from "@/components/forms/Question";
+import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -9,18 +9,16 @@ const AskQuestion = async () => {
 
   if (!userId) redirect("/sign-in");
 
-  console.log(userId)
 
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-      {/* <div className="mt-9">
+      <div className="mt-9">
         <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
-      </div> */}
+      </div>
     </div>
   );
 };
